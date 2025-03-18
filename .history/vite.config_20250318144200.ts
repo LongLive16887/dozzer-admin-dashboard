1f34a0044e3549abc114634f17d707f7680e8e5d
LongLive16887 <mustafoyevnew@gmail.com>
2025-03-18 14:46:10 +0500

    fixed 404 error

---
+++ .history/vite.config_20250318144200.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: true, // Открывает сервер для локальной сети
+    port: 5173, // Можно поменять порт, если надо
+  },
+  { "rewrites: [{ "source": "/(.*)", "destination": "/" }] 
+})
